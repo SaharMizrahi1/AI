f20b4107ea64c478f1670410b4778c4b7621e601
shaked yael <shakedy10@gmail.com>
2023-08-25 10:53:15 +0300

    greedy search

--- GreedyComparator.java
+++ GreedyComparator.java
@@ -4,7 +4,7 @@ public class GreedyComparator implements Comparator<Path> {
 	
 	@Override
 	public int compare(Path p1, Path p2) {
-		// your implementation...
+		// your implementation:
 
 		int heuristicEstimateP1 = p1.getHeuristicEstimate();
 	    int heuristicEstimateP2 = p2.getHeuristicEstimate();
--- GreedySearch.java
+++ GreedySearch.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 public class GreedySearch extends Algorithm {
 
@@ -5,4 +8,20 @@ public class GreedySearch extends Algorithm {
 		super(maze);
 		this.comparator= new GreedyComparator();// set Algorithm::comparator as a GreedyComparator
 	}
+
+	public Solution search() {
+		PriorityQueue<Path> frontier = new PriorityQueue<Path>(comparator);
+		Set<Coordinate> visited = new HashSet<>();
+		// Start state
+		Coordinate startPoint = new Coordinate(0,0);
+		//Goal Point
+		Coordinate endPoint = new Coordinate(maze.getD()-1,maze.getD()-1); // matrix limits - 0 to n-1
+		Path startPath = new Path(maze);
+		startPath.addCoordinate(startPoint);
+		frontier.add(startPath);
+
+
+
+        return null;
+    }
 }

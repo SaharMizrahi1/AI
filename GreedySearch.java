2b797bd2fcbac76a81ab2f768592447dc579de3d
ShakedYael <126869978+ShakedYael@users.noreply.github.com>
2023-08-25 06:25:21 +0000

    greedy

--- GreedySearch.java
+++ GreedySearch.java
@@ -3,6 +3,6 @@ public class GreedySearch extends Algorithm {
 
 	public GreedySearch(Maze maze) {
 		super(maze);
-		// set Algorithm::comparator as a GreedyComparator
+		this.comparator= new GreedyComparator();// set Algorithm::comparator as a GreedyComparator
 	}
 }
